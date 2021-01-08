35be0c2095090c4a3cc7cec7d698ff740767f43b
aaapppsss <adelinebooqw@gmail.com>
2021-01-08 15:10:05 +0800

    search

--- pushtoheroku/public/manualsearch.js
+++ pushtoheroku/public/manualsearch.js
@@ -2,6 +2,8 @@ function searchAddressByName() {
     var name = document.getElementById("search").value;
 
     if (name) {
+        document.getElementById("manualsearchresults").innerHTML = "";
+
         $.ajax({
             url: 'https://developers.onemap.sg/commonapi/search?searchVal=' + name + '&returnGeom=Y&getAddrDetails=Y&pageNum=1',
             success: function (result) {
